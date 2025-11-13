4e30f6b55881f2bade17df2e76c80f0306ee2600
Harrier <sadyukh@gmail.com>
2025-11-13 23:05:02 +0400

    m

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,5 +3,6 @@
 -- vim.cmd.colorscheme 'tokyonight-night'
 -- vim.cmd.colorscheme 'zenwritten'
 -- vim.cmd.colorscheme 'nord'
-vim.cmd.colorscheme 'kanagawa-dragon'
+-- vim.cmd.colorscheme 'kanagawa-dragon'
 -- vim.cmd.colorscheme 'rose-pine-moon'
+vim.cmd.colorscheme 'gruber-darker'
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -725,11 +725,17 @@ require('lazy').setup({
     transparent = true,
   } },
 
-  { 'rose-pine/neovim', name = 'rose-pine', opts = {
-    styles = {
-      transparency = true,
+  { 'blazkowolf/gruber-darker.nvim' },
+
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    opts = {
+      --styles = {
+      --  transparency = true,
+      --},
     },
-  } },
+  },
 
   {
     'folke/tokyonight.nvim',
@@ -833,7 +839,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',

375af0375eca87376303954accc9f0af8425593e
Harrier <sadyukh@gmail.com>
2025-11-13 23:16:29 +0400

    m

--- lua/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- Set colorscheme here
-
--- vim.cmd.colorscheme 'tokyonight-night'
--- vim.cmd.colorscheme 'zenwritten'
--- vim.cmd.colorscheme 'nord'
--- vim.cmd.colorscheme 'kanagawa-dragon'
--- vim.cmd.colorscheme 'rose-pine-moon'
-vim.cmd.colorscheme 'gruber-darker'
